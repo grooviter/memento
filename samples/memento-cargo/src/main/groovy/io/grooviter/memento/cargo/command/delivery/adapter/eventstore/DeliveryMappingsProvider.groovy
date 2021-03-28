8c5607f3616f80cf8010c82e5c45f9941840611d
Mario Garcia <mario.ggar@gmail.com>
2021-03-28 21:11:00 +0200

    fix(eventstore): missing delivery event mapping

--- samples/memento-cargo/src/main/groovy/io/grooviter/memento/cargo/command/delivery/adapter/eventstore/DeliveryMappingsProvider.groovy
+++ samples/memento-cargo/src/main/groovy/io/grooviter/memento/cargo/command/delivery/adapter/eventstore/DeliveryMappingsProvider.groovy
@@ -4,6 +4,7 @@ package io.grooviter.memento.cargo.command.delivery.adapter.eventstore
 import io.grooviter.memento.cargo.command.common.adapter.eventstore.CommandMappingsContainer
 import io.grooviter.memento.cargo.command.delivery.domain.Delivery
 import io.grooviter.memento.cargo.command.delivery.domain.events.Loaded
+import io.grooviter.memento.cargo.command.delivery.domain.events.Prepared
 import io.grooviter.memento.cargo.command.delivery.domain.events.Received
 import io.grooviter.memento.cargo.command.delivery.domain.events.Requested
 import io.grooviter.memento.cargo.command.delivery.domain.events.Unloaded
@@ -20,6 +21,7 @@ class DeliveryMappingsProvider implements Provider<CommandMappingsContainer> {
         Mappings mappings = Mappings.builder()
             .addMapping("DELIVERY", Delivery)
             .addMapping("DELIVERY_REQUESTED", Requested)
+            .addMapping("DELIVERY_PREPARED", Prepared)
             .addMapping("DELIVERY_LOADED", Loaded)
             .addMapping("DELIVERY_UNLOADED", Unloaded)
             .addMapping("DELIVERY_RECEIVED", Received)
