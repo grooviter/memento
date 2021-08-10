a1b4f84239d64d3dbc3ebc153b0da2ad005c5af9
Mario Garcia <mario.ggar@gmail.com>
2021-08-11 00:23:43 +0200

    fix(csv): files are not created if don't exist

--- modules/memento-csv/src/main/groovy/io/grooviter/memento/csv/ext/MementoCsvExtension.groovy
+++ modules/memento-csv/src/main/groovy/io/grooviter/memento/csv/ext/MementoCsvExtension.groovy
@@ -5,10 +5,10 @@ import io.grooviter.memento.impl.MementoBuilder
 
 class MementoCsvExtension {
     static MementoBuilder csvStorage(MementoBuilder builder, File eventsFile, File snapshotsFile) {
-        return builder.eventStorage(new CsvEventStorage(eventsFile, snapshotsFile))
+        return builder.eventStorage(CsvEventStorage.create(eventsFile, snapshotsFile))
     }
 
     static MementoBuilder csvStorage(MementoBuilder builder, String eventsFilePath, String snapshotsFilePath) {
-        return builder.eventStorage(new CsvEventStorage(new File(eventsFilePath), new File (snapshotsFilePath)))
+        return builder.eventStorage(CsvEventStorage.create(new File(eventsFilePath), new File (snapshotsFilePath)))
     }
 }
